5d255247f161a9be00ba369c9f8aef2c31687529
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-10-18 23:30:31 +0700

    Add some dependencies and config them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
+// eslint-disable-next-line no-undef
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        primary: "#fea928",
-        secondary: "#ed8900",
-      },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: "1rem",
-          sm: "3rem",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                primary: "#fea928",
+                secondary: "#ed8900",
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: "1rem",
+                    sm: "3rem",
+                },
+            },
+            backgroundImage: {
+                "banner-login": "url('/src/assets/website/background.jpg')",
+            },
         },
-      },
     },
-  },
-  plugins: [],
+    plugins: [flowbite.plugin()],
 };
