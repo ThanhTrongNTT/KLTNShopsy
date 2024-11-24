a2bcaed38e6ff0dec98161c00bc8808ed93b5af6
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-11-25 00:27:04 +0700

    Update package

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-      "@assets": "/src/assets",
-      "@components": "/src/components",
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": "/src",
+            "@assets": "/src/assets",
+            "@components": "/src/components",
+        },
     },
-  },
 });
