08c830932b295288cc5baa665b6cd5a6ea6b581d
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-11-25 00:29:39 +0700

    Create schema to check resolver for form

---
+++ src/libs/utils/schema.ts
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+export const EmailSchema = Yup.string()
+    .required("Please enter your emaill address!")
+    .matches(
+        // eslint-disable-next-line no-control-regex
+        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+        { message: "Please enter valid email address" }
+    );
+
+export const PasswordSchema = Yup.string()
+    .required("Please enter your password")
+    .min(8, "Password must be 8 characters");
+
+export const ConfirmPasswordSchema = Yup.string()
+    .required("Please enter your confirm password")
+    .min(8, "Password must be 8 characters");
