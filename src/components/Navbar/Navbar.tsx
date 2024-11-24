2b7b9daf6bc73ed57a89099c28964ee5505c477f
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-11-25 00:30:34 +0700

    Update Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,235 +1,226 @@
-import React, { FormEvent, KeyboardEvent, useState } from "react";
 import Logo from "@assets/logo.png";
-import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import React, { FormEvent, KeyboardEvent, useEffect, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { IoMdSearch } from "react-icons/io";
+import { Link, useNavigate } from "react-router-dom";
 import DarkMode from "./DarkMode";
+import DropdownCart from "../dropdown/DropdownCart";
+import Cookies from "js-cookie";
+import AuthenticationApi from "../../libs/api/authentication.api";
+import { toast } from "react-toastify";
+import CategoryApi from "../../libs/api/category.api";
+import { formatCategories } from "../../libs/utils/category";
+import { Category } from "../../data/Categories";
+import { RootState, useAppDispatch, useAppSelector } from "../../redux/store";
+import { clearUser } from "../../redux/slices/userSlice";
 
-const Menu = [
-  {
-    id: 1,
-    name: "Trang chủ",
-    link: "/#",
-  },
-  {
-    id: 2,
-    name: "Nam",
-    link: "/#services",
-    dropdownLinks: [
-      {
+const items = [
+    {
         id: 1,
-        name: "Tất cả sản phẩm",
-        link: "/#",
-      },
-      {
+        name: "T-shirt",
+        price: 25.99,
+        image: "https://readymadeui.com/images/product1.webp", // URL của ảnh sản phẩm
+        quantity: 2, // Số lượng sản phẩm trong giỏ
+    },
+    {
         id: 2,
-        name: "Best Selling",
-        link: "/#",
-      },
-      {
+        name: "Jeans",
+        price: 49.99,
+        image: "https://readymadeui.com/images/product1.webp", // URL của ảnh sản phẩm
+        quantity: 1,
+    },
+    {
         id: 3,
-        name: "Top Rated",
-        link: "/#",
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Nữ",
-    link: "/#",
-    dropdownLinks: [
-      {
-        id: 1,
-        name: "Trending Products",
-        link: "/#",
-      },
-      {
-        id: 2,
-        name: "Best Selling",
-        link: "/#",
-      },
-      {
-        id: 3,
-        name: "Top Rated",
-        link: "/#",
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: "Trẻ em",
-    link: "/#",
-    dropdownLinks: [
-      {
-        id: 1,
-        name: "Trending Products",
-        link: "/#",
-      },
-      {
-        id: 2,
-        name: "Best Selling",
-        link: "/#",
-      },
-      {
-        id: 3,
-        name: "Top Rated",
-        link: "/#",
-      },
-    ],
-  },
-  {
-    id: 5,
-    name: "Trang sức",
-    link: "/#",
-    dropdownLinks: [
-      {
-        id: 1,
-        name: "Trending Products",
-        link: "/#",
-      },
-      {
-        id: 2,
-        name: "Best Selling",
-        link: "/#",
-      },
-      {
-        id: 3,
-        name: "Top Rated",
-        link: "/#",
-      },
-    ],
-  },
+        name: "Sneakers",
+        price: 75.99,
+        image: "https://readymadeui.com/images/product1.webp", // URL của ảnh sản phẩm
+        quantity: 1,
+    },
+    {
+        id: 4,
+        name: "Cap",
+        price: 15.99,
+        image: "https://readymadeui.com/images/product1.webp", // URL của ảnh sản phẩm
+        quantity: 3,
+    },
 ];
 
-const Navbar = ({ handleOrderPopup }) => {
-  const [searchText, setSearchText] = useState("");
-  const handleSearch = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      console.log("Enter: ", searchText);
-      setSearchText("");
-    }
-  };
+const Navbar = () => {
+    const navigate = useNavigate();
+    const [isHovered, setIsHovered] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    const [Menu, setMenu] = useState<Category[]>([]);
+    const { userInfo } = useAppSelector((state: RootState) => state.user);
+    const dispatch = useAppDispatch();
+    const fetchCategories = async () => {
+        const res = await CategoryApi.getAllCategory();
+        setMenu(formatCategories(res.data));
+        console.log("Menu: ", Menu);
+    };
+    const handleSearch = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            console.log("Enter: ", searchText);
+            setSearchText("");
+        }
+    };
+    const handleLogout = () => {
+        AuthenticationApi.logout().then(async (res) => {
+            console.log(res);
+            const allCookies = Cookies.get();
+            dispatch(clearUser());
+            Object.keys(allCookies).forEach((cookieName) => {
+                Cookies.remove(cookieName, { path: "/" });
+            });
+            // await window.location.reload();
+            toast.success("Logout success!", {
+                autoClose: 1000,
+                delay: 10,
+                draggable: true,
+                pauseOnHover: true,
+            });
+        });
+    };
 
-  const handleChangeSearch = (e: FormEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value);
-    const newValue = e.currentTarget.value;
-    setSearchText((prevValue) => prevValue + newValue);
-    console.log(searchText);
-  };
-  return (
-    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
-      {/* upper Navbar */}
-      <div className="bg-primary/40 py-2">
-        <div className="container flex justify-between items-center">
-          <div>
-            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
-              <img src={Logo} alt="Logo" className="w-10" />
-              Shopsy
-            </a>
-          </div>
+    const isLogin = Cookies.get("accessToken");
 
-          {/* search bar */}
-          <div className="flex justify-between items-center gap-4">
-            <div className="relative group hidden sm:block">
-              <input
-                onKeyDown={handleSearch}
-                onChange={handleChangeSearch}
-                type="text"
-                value={searchText}
-                placeholder="search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800 items-center content-center"
-              />
-              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+    const handleChangeSearch = (e: FormEvent<HTMLInputElement>) => {
+        const newValue = e.currentTarget.value;
+        setSearchText(newValue);
+        console.log("Current input: ", newValue);
+    };
+    useEffect(() => {
+        fetchCategories();
+    }, [isLogin]);
+    return (
+        <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+            {/* upper Navbar */}
+            <div className="bg-primary/40 py-2">
+                <div className="container flex justify-between items-center">
+                    <div>
+                        <a
+                            href="#"
+                            className="font-bold text-2xl sm:text-3xl flex gap-2"
+                        >
+                            <img src={Logo} alt="Logo" className="w-10" />
+                            Shopsy
+                        </a>
+                    </div>
+
+                    {/* search bar */}
+                    <div className="flex justify-between items-center gap-4">
+                        <div className="relative group hidden sm:block">
+                            <input
+                                onKeyDown={handleSearch}
+                                onChange={handleChangeSearch}
+                                type="text"
+                                value={searchText}
+                                placeholder="search"
+                                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800 items-center content-center"
+                            />
+                            <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+                        </div>
+
+                        {/* order button */}
+                        <div className="relative">
+                            <button
+                                className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full items-center gap-3 flex group relative"
+                                onMouseEnter={() => setIsHovered(true)}
+                                onMouseLeave={() => setIsHovered(false)}
+                                onClick={() => navigate("/cart")}
+                            >
+                                {/* Icon giỏ hàng với badge số lượng sản phẩm */}
+                                <div className="relative">
+                                    <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+                                    <span className="absolute -top-3 -right-4 bg-red-500 text-white text-xs font-bold rounded-full px-1.5 py-0.5">
+                                        {items.length}
+                                    </span>
+                                </div>
+                            </button>
+                            {isHovered && <DropdownCart items={items} />}
+                        </div>
+                        <div>
+                            <DarkMode />
+                        </div>
+                        {!isLogin ? (
+                            <div className="flex gap-5">
+                                <Link to={"/login"}>
+                                    <div className="text-lg font-medium text-gray-700 hover:text-gray-800 cursor-pointer">
+                                        Login
+                                    </div>
+                                </Link>
+                                <span
+                                    className="h-6 w-px bg-gray-400"
+                                    aria-hidden="true"
+                                ></span>
+                                <Link to={"sign-up"}>
+                                    <div className="text-lg font-medium text-gray-700 hover:text-gray-800 cursor-pointer pr-3">
+                                        Create account
+                                    </div>
+                                </Link>
+                            </div>
+                        ) : (
+                            <div className="flex gap-5">
+                                <span>Logged</span>
+                                <button onClick={handleLogout}>Logout</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
-
-            {/* order button */}
-            <button
-              onClick={() => handleOrderPopup()}
-              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white  py-1 px-4 rounded-full flex items-center gap-3 group"
-            >
-              <span className="group-hover:block hidden transition-all duration-200">
-                Order
-              </span>
-              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
-            </button>
-
-            {/* Darkmode Switch */}
-            <div>
-              <DarkMode />
+            {/* lower Navbar */}
+            <div data-aos="zoom-in" className="flex justify-center">
+                <ul className="sm:flex hidden items-center gap-4">
+                    <li key={1} className="group relative cursor-pointer">
+                        <a href={"/"} className="px-4 flex items-center">
+                            Trang chủ
+                        </a>
+                    </li>
+                    {Menu.map((data) => (
+                        <li
+                            key={data.id}
+                            className="group relative cursor-pointer"
+                        >
+                            <a
+                                href={`/product?gender=${data.categoryName}`}
+                                className="px-4 flex items-center"
+                            >
+                                {data.locale}
+                                {data.children.length > 0 && (
+                                    <span>
+                                        <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+                                    </span>
+                                )}
+                            </a>
+                            {data.children.length > 0 && (
+                                <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+                                    <ul>
+                                        {data.children.map((item) => (
+                                            <li key={item.id}>
+                                                <Link
+                                                    to={`/product?gender=${data.categoryName}&category=${item.categoryName}`}
+                                                    className="inline-block w-full rounded-md p-2 hover:bg-primary/20 "
+                                                >
+                                                    {item.locale}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <div className="flex gap-5">
-              <div className="text-lg font-medium text-gray-700 hover:text-gray-800 cursor-pointer">
-                Login
-              </div>
-              <span className="h-6 w-px bg-gray-400" aria-hidden="true"></span>
-              <div className="text-lg font-medium text-gray-700 hover:text-gray-800 cursor-pointer pr-3">
-                Create account
-              </div>
-            </div>
-          </div>
+            {userInfo.id === "" || (
+                <img
+                    src={userInfo.userProfile.avatar.url}
+                    alt="Logo"
+                    className="w-10 h-10 rounded-full absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
+                />
+            )}
         </div>
-      </div>
-      {/* lower Navbar */}
-      <div data-aos="zoom-in" className="flex justify-center">
-        <ul className="sm:flex hidden items-center gap-4">
-          {Menu.map((data) => (
-            <li key={data.id} className="group relative cursor-pointer">
-              <a
-                href={data.link}
-                className="px-4 hover:text-primary duration-200 flex items-center"
-              >
-                {data.name}
-                {data.dropdownLinks && (
-                  <span>
-                    <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
-                  </span>
-                )}
-              </a>
-              {data.dropdownLinks && (
-                <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
-                  <ul>
-                    {data.dropdownLinks.map((item) => (
-                      <li key={item.id}>
-                        <a
-                          href={item.link}
-                          className="inline-block w-full rounded-md p-2 hover:bg-primary/20 "
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </li>
-          ))}
-          {/* Simple Dropdown and Links */}
-          {/* <li className="group relative cursor-pointer">
-            <a href="#" className="flex items-center gap-[2px] py-2">
-              Trending Products
-              <span>
-                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
-              </span>
-            </a>
-            <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
-                  <ul>
-                    {data.dropdownLinks.map((item) => (
-                      <li key={item.id}>
-                        <a
-                          href={item.link}
-                          className="inline-block w-full rounded-md p-2 hover:bg-primary/20 "
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-          </li> */}
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
