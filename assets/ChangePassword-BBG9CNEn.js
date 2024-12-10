65d6791c944014652181ecb2672ebb9b052cd6d3
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-12-10 13:44:12 +0700

    Updates

---
+++ assets/ChangePassword-BBG9CNEn.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DGMkvBNq.js";const a=()=>s.jsx("div",{children:"ChangePassword"});export{a as default};
--- assets/ChangePassword-D4lFoqpz.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DqBCf9oK.js";const a=()=>s.jsx("div",{children:"ChangePassword"});export{a as default};
