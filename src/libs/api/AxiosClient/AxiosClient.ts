cc84dfd8a1d4cf8ca9347ddeceb251e2656016e1
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-12-14 18:50:46 +0700

    Change api url

--- src/libs/api/AxiosClient/AxiosClient.ts
+++ src/libs/api/AxiosClient/AxiosClient.ts
@@ -4,9 +4,9 @@ import Cookies from "js-cookie";
 import useLoadingStore from "../../../redux/store/loading.store";
 // Please have a look at here `https://github.com/axios/axios#request-config` for the full list of configs
 
-// const baseURLAPI =
-// "https://thanhtrongsport-be-production.up.railway.app/api/v1/";
-const baseURLAPI = "http://localhost:8080/api/v1/";
+const baseURLAPI =
+    "https://thanhtrongsport-be-production.up.railway.app/api/v1/";
+// const baseURLAPI = "http://localhost:8080/api/v1/";
 const AxiosClient = axios.create({
     // baseURL: process.env.REACT_APP_API_URL,
     baseURL: baseURLAPI,
